f1221d7e3f6635e8e02adc1d877744f7f263147e
Michael Kennedy <mikeckennedy@gmail.com>
2014-05-20 10:24:38 +0100

    added applied nosql starter code.

---
+++ conferences/SDDConf/SDDConf_2014/applied_nosql_dotnet/SddConfNoSQLBlog/SddConfNoSQLBlog/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace SddConfNoSQLBlog
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to the SDD NoSQL Blog App by @mkennedy");
+		}
+	}
+
+}
