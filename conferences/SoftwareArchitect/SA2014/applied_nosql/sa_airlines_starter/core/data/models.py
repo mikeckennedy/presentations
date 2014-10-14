70b81f0d563eeee91cd4244e0198f9fe1c96d7c1
Michael Kennedy <mikeckennedy@gmail.com>
2014-10-14 17:00:12 +0100

    Ready for presentation

---
+++ conferences/SoftwareArchitect/SA2014/applied_nosql/sa_airlines_starter/core/data/models.py
@@ -0,0 +1,26 @@
+class Address:
+    pass
+
+
+class Customer:
+    pass
+
+
+class Airline:
+    pass
+
+
+class Flight:
+    pass
+
+    # index_background=True,
+    # indexes=['fields': ['title'], 'name': 'jays_index'}])
+
+
+class Trip:
+    pass
+
+
+class Connection:
+    pass
+
