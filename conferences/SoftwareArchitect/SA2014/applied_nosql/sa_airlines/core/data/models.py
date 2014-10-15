014ea1ebc670f4e5c3fec3bb6ebad223cadcb99f
Michael Kennedy <mikeckennedy@gmail.com>
2014-10-15 12:59:36 +0100

    demos from SA applied nosql

--- conferences/SoftwareArchitect/SA2014/applied_nosql/sa_airlines/core/data/models.py
+++ conferences/SoftwareArchitect/SA2014/applied_nosql/sa_airlines/core/data/models.py
@@ -1,27 +1,46 @@
+from mongoengine import *
 
-class Address:
+
+class Address(EmbeddedDocument):
+    street = StringField(required=True)
+    city = StringField(required=True)
+    country = StringField(required=True)
+    postal_code = StringField(required=True)
+
+
+class Customer(Document):
+    first_name = StringField(required=True)
+    last_name = StringField(required=True)
+    address = EmbeddedDocumentField(Address)
+
+    meta = dict(collection='Customers')
+
+
+class Airline(Document):
+    name = StringField(required=True)
+    meta = dict(collection='Airlines')
+
+
+class Connection(EmbeddedDocument):
+    flight_number = StringField()
+    end_city = StringField()
+
+
+class Flight(Document):
+    number = StringField(required=True)
+    start_city = StringField(required=True)
+    discount_rate = FloatField()
+    end_city = StringField(required=True)
+    airline = ReferenceField(Airline)
+    delay_in_minutes = IntField()
+    passengers = ListField(ReferenceField(Customer))
+    connections = ListField(EmbeddedDocumentField(Connection))
+    meta = dict(collection='Flights',
+                index_background=True,
+                indexes=[{'fields': ['number'], 'name': 'find_by_number'}])
+
+
+class Trip(Document):
     pass
 
-
-class Customer:
-    pass
-
-
-class Airline:
-    pass
-
-
-class Flight:
-    pass
-
-    # index_background=True,
-    # indexes=['fields': ['title'], 'name': 'find_by_title'}])
-
-
-class Trip:
-    pass
-
-
-class Connection:
-    pass
 
