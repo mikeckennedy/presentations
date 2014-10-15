bf95beab4d42d4e847f383af6f0b3f5433588a5e
Michael Kennedy <mikeckennedy@gmail.com>
2014-10-15 16:41:54 +0100

    Initial project for mongodb perf design example.

---
+++ conferences/SoftwareArchitect/SA2014/high_perf_nosql/design/books/books/MongoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace books
+{
+	public class MongoContext : MongoDB.Kennedy.MongoDbDataContext
+	{
+		public MongoContext() :
+			base("SA_Perf_Design")
+		{
+		}
+
+		public IQueryable<Book> Books { get { return base.GetCollection<Book>(); } }
+	}
+
+}
